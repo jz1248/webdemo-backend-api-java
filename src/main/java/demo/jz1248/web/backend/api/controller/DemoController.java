397e87eef74cff88851e9c8d83a3780d1a19bec0
jz1248 <jz1248it@gmail.com>
2023-10-15 20:49:29 +0800

    update

--- src/main/java/demo/jz1248/web/backend/api/controller/DemoController.java
+++ src/main/java/demo/jz1248/web/backend/api/controller/DemoController.java
@@ -1,4 +1,13 @@
 package demo.jz1248.web.backend.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
 }
