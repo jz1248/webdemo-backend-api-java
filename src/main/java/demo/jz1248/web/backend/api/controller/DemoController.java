60bc1ce675500a375f961b7492229d0d0d13823c
jz1248 <jz1248it@gmail.com>
2023-10-20 21:56:32 +0800

    update

--- src/main/java/demo/jz1248/web/backend/api/controller/DemoController.java
+++ src/main/java/demo/jz1248/web/backend/api/controller/DemoController.java
@@ -1,13 +1,15 @@
 package demo.jz1248.web.backend.api.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    @GetMapping({"/", "/hello"})
+    public String hello(@RequestParam(value = "name", required = false) String name) {
+        return StringUtils.isBlank(name) ? "Hello!" : "Hello, " + name + "!";
     }
 }
